7d309100cb943975a44e380fc04c4130674bc65d
ypnngaa-py <ypnngaa-py@users.noreply.github.com>
2017-04-02 16:12:38 -0700

    relative path

--- SignalRegionMut.py
+++ SignalRegionMut.py
@@ -1,12 +1,12 @@
 import sys
 dic={}
-for l in open(sys.argv[1]):
+for l in open('SignalPepInfo.csv'):
     ls=l.strip().split('\t')
     dic[ls[0]]=ls[1]+'\t'+ls[2]+'\t'+ls[3]+'\t'+ls[4]+'\t'+ls[5]
 #f=open('Extended_Mut_on_Sig.csv','w')
 
-f1=open('Extended_Mut_on_Sig_Germline_app.csv','w')
-for r in open('/home/panyang/Dropbox/Project2015/MutationDensity_Project/Dataset_Mutation/sub_Uni_SNPdb_ns_s3.tsv'):
+f1=open('Extended_Mut_on_Sig_Germline_total.csv','w')
+for r in open('../../../MutationDensity_Project/Dataset_Mutation/sub_Uni_SNPdb_ns_s3.tsv'):
     rs=r.strip().split('\t')
     if rs[1] in dic:
         #if int(rs[2])<=int(dic[rs[1]].split('\t')[4]):
@@ -14,8 +14,8 @@ for r in open('/home/panyang/Dropbox/Project2015/MutationDensity_Project/Dataset
             f1.write('\t'.join(rs+dic[rs[1]].split('\t'))+'\n')
 
     
-f2=open('Extended_Mut_on_Sig_Somatic_app.csv','w')
-for r in open('/home/panyang/Dropbox/Project2015/MutationDensity_Project/Dataset_Mutation/sub_Uni_BioMuta3_ns_s3.tsv'):
+f2=open('Extended_Mut_on_Sig_Somatic_total.csv','w')
+for r in open('../../../MutationDensity_Project/Dataset_Mutation/sub_Uni_BioMuta3_ns_s3.tsv'):
     rs=r.strip().split('\t')
     if rs[1] in dic:
         #if int(rs[2])<=int(dic[rs[1]].split('\t')[4]):
